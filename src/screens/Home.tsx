7c6503918cad5bd7ca46822f3437d2959e04685e
Junior Dering <juniordering@hotmail.com>
2023-05-30 20:33:18 -0400

    feat: finished card of exercise

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,61 @@
-import { VStack } from 'native-base'
+import { VStack, FlatList, HStack, Heading, Text } from 'native-base'
 import { HomeHeader } from '../components/HomeHeader'
+import { Group } from '../components/Group'
+import { useState } from 'react'
+import { ExerciseCard } from '../components/ExerciseCard'
 
 export function Home() {
+  const [groups, setGroups] = useState([
+    'costas',
+    'bíceps',
+    'tríceps',
+    'ombros',
+  ])
+  const [exercises, setExercises] = useState([
+    'Puxada frontal',
+    'Remada curvada',
+    'Remada baixa',
+    'Levantamento terras',
+  ])
+  const [groupSelected, setGroupSelected] = useState('costas')
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
     </VStack>
   )
 }
